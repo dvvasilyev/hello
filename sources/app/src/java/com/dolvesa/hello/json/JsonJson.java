64665637dfe04a649de472cb04e575cd1d52b115
Denis Vasilyev <dv@dolvesa.com>
2015-05-07 12:55:54 +0300

    simple json converter

---
+++ sources/app/src/java/com/dolvesa/hello/json/JsonJson.java
@@ -0,0 +1,8 @@
+package com.dolvesa.hello.json;
+
+/**
+ * Created by Denis Vasilyev (dv@dolvesa.com) on 07.05.2015 at 11:41.
+ */
+public class JsonJson {
+  // private static Logger log = LoggerFactory.getLogger(JsonJson.class);
+}
---
+++ sources/app/tests/com/dolvesa/hello/json/JsonJsonTest.java
@@ -0,0 +1,39 @@
+package com.dolvesa.hello.json;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Denis Vasilyev (dv@dolvesa.com) on 07.05.2015 at 11:57.
+ *
+ * JSON-to-JSON transformation test
+ */
+public class JsonJsonTest {
+   private static Logger log = LoggerFactory.getLogger(JsonJsonTest.class);
+
+  private JsonJson jsonJson;
+
+  @BeforeClass
+  public void init() {
+    jsonJson = new JsonJson();
+  }
+
+  @Test
+  public void testSourceJson() {
+    String res = jsonJson.sourceToJson();
+    log.debug("source JSON is {}", res);
+    Assert.assertTrue(res.contains("name"));
+    Assert.assertTrue(res.contains("_amount_"));
+  }
+
+  @Test
+  public void testTargetJson() {
+    String res = jsonJson.targetToJson();
+    log.debug("target JSON is {}", res);
+    Assert.assertTrue(res.contains("title"));
+    Assert.assertTrue(res.contains("_amount_"));
+  }
+}
